4b1e9e944673f615704712fa853e375a50282b7f
Luan-Marcelino <luanmarcelinocontato@gmail.com>
2025-08-12 18:22:58 -0300

    logica funcionando

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+let nome = document.querySelector('#amigo');
+let btnAdicionarAmigo = document.querySelector('.button-add');
+let lista = document.querySelector('#listaAmigos');
+
+function adicionarAmigo() {
+    if(nome.value !== '') {
+        amigos.push(nome.value);
+        nome.value = '';
+        nome.focus();
+        colocarNaLista();
+        
+        console.log(amigos);      
+          
+    }else{
+        alert('Por favor, insira um nome.');
+    }
+}
+
+function colocarNaLista() {
+    let lista = document.querySelector('#listaAmigos');
+    lista.innerHTML = '';
+    
+    for(let i = 0; i < amigos.length; i++) {
+        lista.innerHTML += `<li>${amigos[i]}</li>`;   
+    }
+}
+
+function sortearAmigo() {
+    if(amigos.length > 0) {
+        let resultado = document.querySelector('#resultado');
+        resultado.innerHTML = nomeAleatorio();
+        
+    }else {
+        alert('Nenhum nome adicionado.');
+    }
+}
+
+function nomeAleatorio() {
+    let indice = Math.floor(Math.random() * amigos.length);
+    return amigos[indice];
+}
